265f76e7603370dee548af61a5e52aac1bbbeae3
LuisMota93 <luisffsmota@hotmail.com>
2022-03-18 15:31:24 +0000

    Added image field to recipe model.

--- app/app/settings.py
+++ app/app/settings.py
@@ -124,7 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = '/vol/we/media' # tells django where to store all the media files
+STATIC_ROOT = '/vol/web/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
