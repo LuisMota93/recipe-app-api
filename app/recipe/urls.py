3ca0f682c4e40b4b53f143bb046ed8de72d7fe67
LuisMota93 <luisffsmota@hotmail.com>
2022-03-18 10:45:10 +0000

    Added list ingredients feature.

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -6,6 +6,7 @@ from recipe import views
 
 router = DefaultRouter()
 router.register('tags', views.TagViewSet) # registers the viewset with the router
+router.register('ingredients', views.IngredientViewSet)
 
 app_name = 'recipe'
 
